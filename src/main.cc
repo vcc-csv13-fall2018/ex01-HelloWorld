53c23551a5b478da6d17ed05f0b2b99902f8f821
profknight <ahknight@pipeline.sbcc.edu>
2018-09-02 15:34:37 -0700

    Testing

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 /**
  * CS V13 Object-Oriented Programming
- * CRN: 71235
+ * CRN: 71236
  * Assignment: EX01-HelloWorld
  *
  * Statement of code ownership: I hereby state that I have written all of this
