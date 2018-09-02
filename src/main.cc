0b1331ea74ed4d2905e6d952575bc5fd413d98f2
profknight <ahknight@pipeline.sbcc.edu>
2018-09-02 15:20:25 -0700

    testing build workflow

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/**
+ * CS V13 Object-Oriented Programming
+ * CRN: 71234
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author aknight@vcccd.edu
+ */
+
+#include <iostream>
+#include <csv13>
+
+int main(UNUSED(int argc), UNUSED(char *argv[])) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
